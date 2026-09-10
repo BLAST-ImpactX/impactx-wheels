8d66def7ff287f697ce69d8f587b12c745c8ad2e
Axel Huebl <axel.huebl@plasma.ninja>
2026-09-09 18:05:31 -0700

    Release: 26.09

--- library_builders.sh
+++ library_builders.sh
@@ -81,7 +81,7 @@ function install_buildessentials {
 function build_amrex {
     if [ -e amrex-stamp ]; then return; fi
 
-    AMREX_VERSION="26.08"
+    AMREX_VERSION="26.09"
 
     curl ${CURL_RETRY} -fsSL -o amrex-${AMREX_VERSION}.tar.gz \
         https://github.com/AMReX-Codes/amrex/releases/download/${AMREX_VERSION}/amrex-${AMREX_VERSION}.tar.gz
